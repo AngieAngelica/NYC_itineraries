1045b8c59842b9ace156a33c5edbb9098acee276
AngieAngelica <38296176+AngieAngelica@users.noreply.github.com>
2018-10-13 18:29:10 -0400

    Update scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+
+class NYCItin::Scraper
+  @@list = []
+  
+  def self.list
+    @@list
+  end
+  
+  def self.page(url)
+    Nokogiri::HTML(open(url))
