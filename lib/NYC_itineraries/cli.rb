27187963cf62d1a841228117291d7c0301828cbe
Angie <anasab12@gmail.com>
2018-10-14 21:46:34 -0400

    fixed cli

--- lib/NYC_itineraries/cli.rb
+++ lib/NYC_itineraries/cli.rb
@@ -1,23 +1,27 @@
 class NYCItineraries::CLI
 
     def call
-        puts "        Welcome to Frommer's Suggested Itineraries in New York City Gem!"
-        puts "        ________________________________________________________________"
-        puts "                                                                        "
-
         list_itineraries
+        prompt_for_user
     end
 
     def list_itineraries
+        puts "        Welcome to Frommer's Suggested Itineraries in New York City Gem!"
+        puts "        ________________________________________________________________"
+        puts "                                                                        "
         puts <<-DOC
-        Please select the itinerary that you are interested in:
-
+        
         1 - NYC in One Day
         2 - NYC in Two Days
         3 - NYC in Three Days
         4 - NYC for Food and Wine Lovers
-        DOC
 
+        DOC
+       
+    end
+
+    def prompt_for_user
+        puts "        Please select the number of itinerary that you are interested in:"
     end
 
 end
