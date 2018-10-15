edb53ddd408900263e1a09eb05d076369706d9de
Angie <anasab12@gmail.com>
2018-10-14 22:36:24 -0400

    more work on interface

--- lib/NYC_itineraries/cli.rb
+++ lib/NYC_itineraries/cli.rb
@@ -3,6 +3,7 @@ class NYCItineraries::CLI
     def call
         list_itineraries
         prompt_for_user
+        goodbye
     end
 
     def list_itineraries
@@ -17,11 +18,26 @@ class NYCItineraries::CLI
         4 - NYC for Food and Wine Lovers
 
         DOC
-       
     end
 
     def prompt_for_user
-        puts "        Please select the number of itinerary that you are interested in:"
+        input = nil
+        while input != "exit"
+            puts "        Please select the number of itinerary that you are interested in or type exit:"
+        input = gets.strip.downcase
+          case input
+          when "1"
+            puts "More info 1"
+          when "2"
+            puts "More info 2 "
+          when "3"
+            puts "More info 3"
+          end
+        end
+    end
+
+    def goodbye
+      puts "Enjoy your stay in NYC! Bye!"
     end
 
 end
