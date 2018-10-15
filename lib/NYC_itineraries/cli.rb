84569d2da20d2bedd47b8910d5523a9d4c53a41e
Angie <anasab12@gmail.com>
2018-10-14 21:06:01 -0400

    added the welcome message in cli.rb

--- lib/NYC_itineraries/cli.rb
+++ lib/NYC_itineraries/cli.rb
@@ -1,5 +1,11 @@
 class NYCItineraries::CLI
     def call
-        puts "Hello"
+        puts "Welcome to Frommer's Suggested Itineraries in New York City Gem!
+        Please select a number for the itinerary you are interested in:
+        1 - NYC In One Day
+        2 - NYC In Two Days
+        3 - NYC In Three Days
+        4 - NYC For Food and Wine Lovers
+        "
     end
 end
