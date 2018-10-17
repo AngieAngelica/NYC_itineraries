0a0054380e30ef23b6751ac263c365d3b059e17c
Angie <anasab12@gmail.com>
2018-10-16 21:29:53 -0400

    Modifications to executable.rb

--- bin/executable.rb
+++ bin/executable.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
 require_relative "../lib/NYC_itineraries.rb" #don't forget the two dots in front of ../lib to get out of the bin
+require "bundler/setup"
+require "NYC_itineraries"
 
 NYCItineraries::CLI.new.call
--- lib/NYC_itineraries/itinerary.rb
+++ lib/NYC_itineraries/itinerary.rb
@@ -1,16 +1,17 @@
 class NYCItineraries::Length
+    attr_accessor :name, :url 
 
   def self.type
     # I should return a bunch of instances of Lengths
-    puts "        Welcome to Frommer's Suggested Itineraries in New York City Gem!"
-    puts "        ________________________________________________________________"
-    puts "                                                                        "
-    puts <<-DOC
-        1 - NYC in One Day
-        2 - NYC in Two Days
-        3 - NYC in Three Days
-        4 - NYC for Food and Wine Lovers
-        DOC
+    # puts "        Welcome to Frommer's Suggested Itineraries in New York City Gem!"
+    # puts "        ________________________________________________________________"
+    # puts "                                                                        "
+    # puts <<-DOC
+    #     1 - NYC in One Day
+    #     2 - NYC in Two Days
+    #     3 - NYC in Three Days
+    #     4 - NYC for Food and Wine Lovers
+    #     DOC
 
     one_day = self.new
     one_day.name = "In One Day in New York City"
@@ -28,5 +29,6 @@ class NYCItineraries::Length
     food_and_wine.name = "For Food and Wine Lovers in New York City"
     food_and_wine.url = "https://www.frommers.com/destinations/new-york-city/suggested-itineraries/for-food-and-wine-lovers"
 
+    [one_day, two_days, three_days, food_and_wine]
   end
 end
