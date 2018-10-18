daeb0eb8b93dd41ef656e8725a75f266e940dc65
Angie Kassab <anasab12@gmail.com>
2018-10-18 21:11:40 +0000

    more changes to the gemspec

--- bin/executable.rb
+++ bin/executable.rb
@@ -1,7 +1,13 @@
 #!/usr/bin/env ruby
 
-require_relative "../lib/NYC_itineraries.rb" #don't forget the two dots in front of ../lib to get out of the bin
-require "bundler/setup"
-require "NYC_itineraries"
+# require_relative "../lib/NYC_itineraries.rb" #don't forget the two dots in front of ../lib to get out of the bin
+# require "config/environment.rb"
+# require "NYC_itineraries"
+#
+# NYCItineraries::CLI.new.call
+
+#!/usr/bin/env ruby
+
+require_relative '../config/environment.rb'
 
 NYCItineraries::CLI.new.call
